d995c8c72c20fc69cf9d2669aa9ebe02377c9c51
AmirHKarimi888 <amirhkarimi888@gmail.com>
2023-05-18 15:31:32 +0330

    Header was created

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
-
+import  HeaderView  from './components/HeaderView.vue'
 </script>
 
 <template>
   <v-app>
+    <HeaderView />
     <RouterView />
   </v-app>
 </template>
---
+++ src/components/HeaderView.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from "vue";
+
+const drawer = ref(null);
+</script>
+
+<template>
+  <header class="header">
+    <v-app-bar color="grey-darken-4" density="compact">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>Chat App</v-app-bar-title>
+
+      <template v-slot:append>
+        <v-avatar>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/john.jpg"
+            alt="John"
+          ></v-img>
+        </v-avatar>
+      </template>
+    </v-app-bar>
+
+    <v-navigation-drawer color="grey-darken-4" v-model="drawer" temporary>
+      <v-list-item class="bg-grey-darken-3 pt-5 pb-5">
+        <v-avatar>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/john.jpg"
+            alt="John"
+          ></v-img>
+        </v-avatar>
+        <h4 class="mt-4">John</h4>
+        <h5 class="">john@email.com</h5>
+        <h5></h5>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item
+          prepend-icon="mdi-view-dashboard"
+          title="Home"
+          value="home"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-forum"
+          title="About"
+          value="about"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </header>
+</template>
