06f692a069d78c20f5c763d4012701c63e8ccdaa
AmirHKarimi888 <amirhkarimi888@gmail.com>
2023-05-23 18:27:48 +0330

    User searching system was created

--- src/components/FindUsersView.vue
+++ src/components/FindUsersView.vue
@@ -1,32 +1,72 @@
-<script>
-export default {
-  data: () => ({
-    loaded: false,
-    loading: false,
-  }),
+<script setup>
+import { ref } from "vue";
+import { Action } from "../httpService";
+import { url } from "../api";
 
-  methods: {
-    onClick() {
-      this.loading = true;
+const showUsers = ref(false);
+const loaded = ref(false);
+const loading = ref(false);
 
-      setTimeout(() => {
-        this.loading = false;
-        this.loaded = true;
-      }, 2000);
-    },
-  },
+const credential = ref("");
+const users = ref([]);
+
+const onClick = () => {
+  Action.get(url + "/users", (response) => (users.value = response.data)).then(
+    () => {
+      users.value = users.value.filter((user) => {
+        if (
+          user.username.includes(credential.value) ||
+          user.username
+            .toLowerCase()
+            .includes(credential.value.toLowerCase()) ||
+          user.username
+            .toUpperCase()
+            .includes(credential.value.toUpperCase()) ||
+          user.email.includes(credential.value)
+        ) {
+          return user;
+        }
+      });
+    }
+  );
+  loading.value = true;
+
+  setTimeout(() => {
+    loading.value = false;
+    loaded.value = true;
+    showUsers.value = true;
+  }, 1000);
 };
 </script>
 
 <template>
-  <v-text-field
-    :loading="loading"
-    density="compact"
-    variant="solo"
-    label="Find Users"
-    append-inner-icon="mdi-magnify"
-    single-line
-    hide-details
-    @click:append-inner="onClick"
-  ></v-text-field>
+  <div>
+    <v-text-field
+      v-model="credential"
+      :loading="loading"
+      density="compact"
+      variant="solo"
+      label="Find Users"
+      append-inner-icon="mdi-magnify"
+      single-line
+      hide-details
+      @click:append-inner="onClick"
+    ></v-text-field>
+
+    <v-card
+      v-if="showUsers"
+      color="grey-darken-2"
+      class="mx-auto px-1 py-1 mt-1"
+      max-width="344"
+    >
+      <v-list density="compact" nav>
+        <v-list-item v-for="user in users" :key="user.username" value="edit">
+          <v-avatar>
+            <v-img :src="user.avatar"></v-img>
+          </v-avatar>
+          <v-title class="ms-12">{{ user.username }}</v-title>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </div>
 </template>
